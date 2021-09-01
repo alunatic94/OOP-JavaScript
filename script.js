255136f001b4f376d3be6ff0786e41ae13f8da79
alunatic94 <Alberto.luna.642@my.csun.edu>
2021-09-01 11:34:48 -0700

    constructor and prototype demo

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
 'use strict';
+/*      Constructors        */
+const Person=function(firstName,birthYear){
+    this.firstName=firstName;
+    this.birthYear=birthYear;
+}
+
+const jonas=new Person('Jonas,1991');
+
+
+/*      Prototypes        */
+Person.prototype.calcAge=function(){
+    console.log(2037-this.birthYear);
+}
+
+jonas.calcAge();
+//Person.prototype is the prototype of jonas
+console.log(jonas.__proto__);
+console.log(jonas.__proto__ === Person.prototype);
+console.log(Person.prototype.isPrototypeOf(jonas));
+
+Person.prototype.species='Homo Sapiends';
