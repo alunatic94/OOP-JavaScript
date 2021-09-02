0ef559b0d1a4657f29219dbc50017e983b18394c
alunatic94 <Alberto.luna.642@my.csun.edu>
2021-09-02 11:52:16 -0700

    finished constructor section

--- script.js
+++ script.js
@@ -20,3 +20,21 @@ console.log(jonas.__proto__ === Person.prototype);
 console.log(Person.prototype.isPrototypeOf(jonas));
 
 Person.prototype.species='Homo Sapiends';
+
+/*      Prototype Inheritance and Chain        */
+
+console.log(jonas.__proto__);//prototype of Jonas Object
+console.log(jonas.__proto__.__proto__);//prototype 
+
+console.dir(Person.prototype.constructor);//dir gives actual object
+
+const arr=[3,6,4,5,6,9,3,9,6];
+console.log(arr.__proto__);
+console.log(arr.__proto__===Array.prototype);//true
+
+console.log(arr.__proto__.__proto__);//this should be the Array Object
+Array.prototype.unique=function(){
+    [...new Set(this)];
+};
+
+console.log(arr.unique());
